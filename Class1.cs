1853b467e6e9522b855ab937fc8c2a0ee3699977
Sinistral2099 <shirizaan@gmail.com>
2023-11-08 12:14:38 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using Streamer.bot.Common;
+using Streamer.bot.Plugin.Interface;
+using Twitch.Common;
+
+namespace Streamer.botClassLibrary
+{
+    public class Class1
+    {
+
+    }
+}
